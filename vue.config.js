abecef9aa37dd35a60bff1b83f91785467f6367e
kisstest <kisstest2@gmail.com>
2021-02-21 17:27:16 +0900

    add setting on vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 module.exports = {
   devServer: {
     host: 'localhost',
     port: 3333,
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src/'),
+      },
+    },
+    devtool: 'source-map',
+    performance: {
+      hints: false,
+    },
   },
 };
