fe4e0f7c56b49a8209e8132c2833dc0fe03acb63
kisstest <kisstest2@gmail.com>
2021-02-21 15:19:45 +0900

    set [eslint + prettier] with vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,27 @@ module.exports = {
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint"
   ],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
+    'prettier/prettier': [
+      'error',
+      // 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+      // https://prettier.io/docs/en/options.html
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   },
